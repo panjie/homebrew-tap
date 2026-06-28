0e39dd8cdd671f73e8a5f957ae875730f40b4ae6
panjie <panjie@outlook.com>
2026-06-28 08:08:54 +0800

    Update mods to 2.5.0

--- Formula/mods.rb
+++ Formula/mods.rb
@@ -1,6 +1,7 @@
 class Mods < Formula
   desc "AI agent for your terminal"
   homepage "https://github.com/panjie/mods"
+  version "2.5.0"
   license "MIT"
   head "https://github.com/panjie/mods.git", branch: "main" do
     depends_on "go" => :build
@@ -8,23 +9,23 @@ class Mods < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/panjie/mods/releases/download/v2.0.0/mods_2.0.0_Darwin_arm64.tar.gz"
-      sha256 "8e836bc47693fb82028c079efee2d55833350d0a0d4009f83f17a75af39d4258"
+      url "https://github.com/panjie/mods/releases/download/v2.5.0/mods_2.5.0_Darwin_arm64.tar.gz"
+      sha256 "b17d868b52d16de8d572000f1f82f8f1e6e61639a8235960a393812d70efda78"
     end
     on_intel do
-      url "https://github.com/panjie/mods/releases/download/v2.0.0/mods_2.0.0_Darwin_x86_64.tar.gz"
-      sha256 "e606e21f3eca6d509f23eeb9744848305e4e58c2cf269e22f49c528d76e4c496"
+      url "https://github.com/panjie/mods/releases/download/v2.5.0/mods_2.5.0_Darwin_x86_64.tar.gz"
+      sha256 "3bffb08f525fe5a788394d8f1ca90e95454fb9144423432905e57e2fd900932d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/panjie/mods/releases/download/v2.0.0/mods_2.0.0_Linux_arm64.tar.gz"
-      sha256 "7e4458dd4f92e237f94a9e55a95eb85ce6a65b154acdcde3db2a8fc4e0141c27"
+      url "https://github.com/panjie/mods/releases/download/v2.5.0/mods_2.5.0_Linux_arm64.tar.gz"
+      sha256 "f94b23d6ccd42315198f6b440975a64d18a983da5154de8864836a165fd70774"
     end
     on_intel do
-      url "https://github.com/panjie/mods/releases/download/v2.0.0/mods_2.0.0_Linux_x86_64.tar.gz"
-      sha256 "3f3eb9ca65cc9e557c8f20716c709d1b352ec29d7d76f0ea9a99567c474edff9"
+      url "https://github.com/panjie/mods/releases/download/v2.5.0/mods_2.5.0_Linux_x86_64.tar.gz"
+      sha256 "c4582a63f33db64945c4bc7b7d1bac614090274fcc93e1e5e8a6b691bb0fee71"
     end
   end
 
